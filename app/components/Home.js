0b1e48b2fb07e09124e27fc448e926bcad307d05
Michielnuyts <michielnuyts@gmail.com>
2017-04-30 21:42:41 +0200

    basics

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,37 @@
 // @flow
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Home.css'
+import styled from 'styled-components'
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<Heading contentEditable>
+					You are a great Developer!
+				</Heading>
+			</Container>
+		)
+	}
 }
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background-color: tomato;
+`
+
+const Heading = styled.h1`
+	font-size: 2rem;
+	color: tomato;
+	font-weight: bold;
+	text-tranform: uppercase;
+	background-color: cornsilk;
+	padding: 2rem;
+	border: 3px solid white;
+	border-radius: 1rem;
+	margin-top: 1rem;
+`
--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,11 +1,23 @@
 // @flow
-import React, { Component } from 'react';
-import Home from '../components/Home';
+import React, { Component } from 'react'
+import Home from '../components/Home'
+import styled from 'styled-components'
 
 export default class HomePage extends Component {
-  render() {
-    return (
-      <Home />
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<Home />
+			</Container>
+		)
+	}
 }
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	background-color: tomato;
+  height: 100vh;
+`
